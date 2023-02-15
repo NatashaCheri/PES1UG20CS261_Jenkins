e37755e4959f616f00c8911558d7cb9dcbbaf2b4
NatashaCheri <79034820+NatashaCheri@users.noreply.github.com>
2023-02-15 10:34:28 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,8 +6,8 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "My SRN is PES1UG20CS261";
-  cout << "My name is Natasha Cheri Satish";
-  cout << "This indicates the code is edited";
+  cout << "My SRN is PES1UG20CS261\n";
+  cout << "My name is Natasha Cheri Satish\n";
+  cout << "This indicates the code is edited\n";
   return 0;
 }
